10a8cb9c50d9fc3643678c78b2717e7a9cf92058
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2023-06-04 23:44:35 +0300

    Realized window application, which open and read data about threads

--- IPP_Lb1_Brigade6/MainWindow.Designer.cs
+++ IPP_Lb1_Brigade6/MainWindow.Designer.cs
@@ -29,11 +29,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBoxWord = new System.Windows.Forms.GroupBox();
+            this.dataGridViewWord = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBoxExcel = new System.Windows.Forms.GroupBox();
+            this.dataGridViewExcel = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBoxWord.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWord)).BeginInit();
+            this.groupBoxExcel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewExcel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxWord, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxExcel, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 337F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(660, 337);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBoxWord
+            // 
+            this.groupBoxWord.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxWord.Controls.Add(this.dataGridViewWord);
+            this.groupBoxWord.Location = new System.Drawing.Point(333, 3);
+            this.groupBoxWord.Name = "groupBoxWord";
+            this.groupBoxWord.Size = new System.Drawing.Size(324, 331);
+            this.groupBoxWord.TabIndex = 1;
+            this.groupBoxWord.TabStop = false;
+            this.groupBoxWord.Text = "Word";
+            // 
+            // dataGridViewWord
+            // 
+            this.dataGridViewWord.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewWord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewWord.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.dataGridViewTextBoxColumn1, this.dataGridViewTextBoxColumn2 });
+            this.dataGridViewWord.Location = new System.Drawing.Point(6, 28);
+            this.dataGridViewWord.Name = "dataGridViewWord";
+            this.dataGridViewWord.Size = new System.Drawing.Size(312, 297);
+            this.dataGridViewWord.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Параметр";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 100;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.HeaderText = "Значення";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 100;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // groupBoxExcel
+            // 
+            this.groupBoxExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxExcel.Controls.Add(this.dataGridViewExcel);
+            this.groupBoxExcel.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxExcel.Name = "groupBoxExcel";
+            this.groupBoxExcel.Size = new System.Drawing.Size(324, 331);
+            this.groupBoxExcel.TabIndex = 0;
+            this.groupBoxExcel.TabStop = false;
+            this.groupBoxExcel.Text = "Excel";
+            // 
+            // dataGridViewExcel
+            // 
+            this.dataGridViewExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewExcel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewExcel.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.Column1, this.Column2 });
+            this.dataGridViewExcel.Location = new System.Drawing.Point(6, 28);
+            this.dataGridViewExcel.Name = "dataGridViewExcel";
+            this.dataGridViewExcel.Size = new System.Drawing.Size(312, 297);
+            this.dataGridViewExcel.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Параметр";
+            this.Column1.MinimumWidth = 100;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Значення";
+            this.Column2.MinimumWidth = 100;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MinimumSize = new System.Drawing.Size(700, 400);
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Threads";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBoxWord.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWord)).EndInit();
+            this.groupBoxExcel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewExcel)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridView dataGridViewWord;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+
+        private System.Windows.Forms.GroupBox groupBoxExcel;
+        private System.Windows.Forms.DataGridView dataGridViewExcel;
+
+        private System.Windows.Forms.GroupBox groupBoxWord;
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 
         #endregion
     }
--- IPP_Lb1_Brigade6/MainWindow.cs
+++ IPP_Lb1_Brigade6/MainWindow.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,89 @@ namespace IPP_Lb1_Brigade6
 {
     public partial class MainWindow : Form
     {
+        private void StartExcel()
+        {
+            try
+            {
+                var excel1Process = Process.Start("C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE");
+
+                while (true)
+                {
+                    if (excel1Process.HasExited)
+                    {
+                        dataGridViewExcel.Rows.Clear();
+                        dataGridViewExcel.Rows.Add("Процесс завершенно!");
+                        break;
+                    }
+                    else
+                    {
+                        var totalProcessorTime = DateTime.Now - excel1Process.StartTime;
+                        var userProcessorTime = totalProcessorTime - excel1Process.UserProcessorTime;
+                        var privilegedProcessorTime = totalProcessorTime - excel1Process.PrivilegedProcessorTime;
+                        // Виведення відомостей про час виконання потоку
+                        dataGridViewExcel.Rows.Clear();
+                        dataGridViewExcel.Rows.Add("ID процесу", excel1Process.Id);
+                        dataGridViewExcel.Rows.Add("Загальний час виконання", totalProcessorTime);
+                        dataGridViewExcel.Rows.Add("Час виконання частини користувача", userProcessorTime);
+                        dataGridViewExcel.Rows.Add("Час виконання частини ядра", privilegedProcessorTime);
+                        Thread.Sleep(5000);
+                    }
+                }
+            }
+            catch
+            {
+                dataGridViewExcel.Rows.Clear();
+                dataGridViewExcel.Rows.Add("Не можу запустити Excel!");
+            }
+        }
+
+        private void StartWord()
+        {
+            try
+            {
+                var word2Process = Process.Start("C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE");
+
+                while (true)
+                {
+                    if (word2Process.HasExited)
+                    {
+                        dataGridViewWord.Rows.Clear();
+                        dataGridViewWord.Rows.Add("Процесс завершенно!");
+                        break;
+                    }
+                    else
+                    {
+                        var totalProcessorTime = DateTime.Now - word2Process.StartTime;
+                        var userProcessorTime = totalProcessorTime - word2Process.UserProcessorTime;
+                        var privilegedProcessorTime = totalProcessorTime - word2Process.PrivilegedProcessorTime;
+                        // Виведення відомостей про час виконання потоку
+                        dataGridViewWord.Rows.Clear();
+                        dataGridViewWord.Rows.Add("ID процесу", word2Process.Id);
+                        dataGridViewWord.Rows.Add("Загальний час виконання", totalProcessorTime);
+                        dataGridViewWord.Rows.Add("Час виконання частини користувача", userProcessorTime);
+                        dataGridViewWord.Rows.Add("Час виконання частини ядра", privilegedProcessorTime);
+                        Thread.Sleep(5000);
+                    }
+                }
+            }
+            catch
+            {
+                dataGridViewWord.Rows.Clear();
+                dataGridViewWord.Rows.Add("Не можу запустити Word!");
+            }
+        }
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            var threadExcel = new Thread(StartExcel);
+            var threadWord = new Thread(StartWord);
+            threadExcel.Start();
+            threadWord.Start();
         }
     }
 }
